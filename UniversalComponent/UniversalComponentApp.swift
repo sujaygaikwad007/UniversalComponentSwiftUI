1795149bff9707c8b161ec5bed35cbfbb02e9a46
Aniket Patil <aniket.p@reapmind.com>
2024-07-05 17:23:17 +0530

    Initial Commit

---
+++ UniversalComponent/UniversalComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalComponentApp.swift
+//  UniversalComponent
+//
+//  Created by Aniket Patil on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct UniversalComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
