b2e298b86988525531f61feaed6e5c1705df7025
sujaygaikwad007 <gaikwadsujay007@gmail.com>
2025-02-24 10:33:52 +0530

    Add textField

--- UniversalComponent/Components/TextField/CustomTextField.swift
+++ UniversalComponent/Components/TextField/CustomTextField.swift
@@ -1,51 +1,94 @@
 import SwiftUI
 
+enum TextFieldStyle {
+    case bordered
+    case borderless
+}
+
 struct CustomTextField: View {
-    let title: String
-    let placeholder: String
     @Binding var text: String
+    let title: String
+    let required: String
+    let placeholder: String
     let isSecure: Bool
-    let keyboardType: UIKeyboardType  // Changed from String to UIKeyboardType
-    
+    let keyboardType: UIKeyboardType
+    let height: CGFloat?
+    let unit: String
+    let style: TextFieldStyle
+    let helperText: String
+    let foregroundColor: Color?
+    let backgroundColor : Color?
+
     var body: some View {
-        VStack(spacing: 5) {
-            Text(title)
-                .font(.caption)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            
-            if isSecure {
-                SecureField(placeholder, text: $text)
+        VStack(spacing: 10) {
+            HStack {
+                Text(title)
                     .font(.caption)
-                    .foregroundColor(.black)
-                    .padding()
-                    .clipShape(Capsule())
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 0.5)
-                    )
-            } else {
-                TextField(placeholder, text: $text)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                Text(required)
                     .font(.caption)
-                    .foregroundColor(.black)
-                    .autocapitalization(.none)
-                    .keyboardType(keyboardType)
-                    .padding()
-                    .clipShape(Capsule())
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 0.5)
-                    )
+                    .foregroundColor(.gray)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+
+            switch style {
+            case .bordered:
+                ZStack(alignment: .trailing) {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.gray, lineWidth: 0.5)
+                        .frame(height: height)
+
+                    if isSecure {
+                        SecureField(placeholder, text: $text)
+                            .font(.caption)
+                            .foregroundColor(foregroundColor)
+                            .keyboardType(keyboardType)
+                            .padding(.horizontal, 10)
+                            .frame(height: height)
+                    } else {
+                        TextField(placeholder, text: $text)
+                            .font(.caption)
+                            .foregroundColor(foregroundColor)
+                            .keyboardType(keyboardType)
+                            .padding(.horizontal, 10)
+                            .frame(height: height)
+                    }
+
+                    Text(unit)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                        .padding(.trailing, 15)
+                }
+                .frame(height: height)
+                .background(backgroundColor)
+                
+                // Display helper text below the text field if available
+                if let helperText = helperText, !helperText.isEmpty {
+                    Text(helperText)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+
+            case .borderless:
+                if isSecure {
+                    SecureField(placeholder, text: $text)
+                        .font(.caption)
+                        .foregroundColor(foregroundColor)
+                        .keyboardType(keyboardType)
+                        .multilineTextAlignment(.trailing)
+                } else {
+                    TextField(placeholder, text: $text)
+                        .font(.caption)
+                        .foregroundColor(foregroundColor)
+                        .keyboardType(keyboardType)
+                        .multilineTextAlignment(.trailing)
+                }
             }
         }
     }
 }
 
-struct CustomTextField_Previews: PreviewProvider {
-    @State static var text: String = ""
-    
-    static var previews: some View {
-        CustomTextField(title: "Email", placeholder: "Enter your email", text: $text, isSecure: false, keyboardType: .emailAddress)
-            .padding()
-           
-    }
-}
+
